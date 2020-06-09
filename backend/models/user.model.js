236889408a40ffc9f630e2a0eced2c991816d0a1
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-06-09 14:33:55 -0400

    Add Database Connection and Create Collection/Tables

---
+++ backend/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema =  new Schema({
+    username: {
+        type:String,
+        required:true,
+        unique:true,
+        trim:true, 
+        minlength: 3
+    }, },
+    {
+        timestamps:true
+    }
+    );
+
+    const User = mongoose.model("User", userSchema);
+
+    module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 
 require("dotenv").config();
 
@@ -10,6 +12,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true});
+
+const connection = mongoose.connection;
+connection.once("open", function(){
+    console.log("MongoDB databse connection established successfully!");
+    
+})
 
 app.listen(port, function(){
     console.log("Server is running on port: 5000");   
