0eb8550e5fe5242870cd89ddb5f1d34c6928d858
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-06-10 12:32:49 -0400

    Add test component files

---
+++ src/components/create-user.component.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+
+export default class CreateUser extends Component{
+
+    render(){
+        return (
+            <div>
+                <p> You are on Exercises list component!</p>
+            </div>
+        )
+    }
+}
